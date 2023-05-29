18b06b317ece8df911ae9e119a66e251a8d2f4b7
Fausto Castagnari Marouvo <fausto.marouvo@btgpactual.com>
2023-05-29 12:26:59 -0300

    Melhorias no código e layout

---
+++ Bitcoin/Application/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  Bitcoin
+//
+//  Created by Fausto Marouvo on 29/05/23.
+//
+
+import UIKit
+
+final class Constants {
+
+    class Size {
+        static let small: CGFloat = 4
+        static let medium: CGFloat = 20
+        static let big: CGFloat = 64
+        static let top: CGFloat = 100
+    }
+
+    class Color {
+        static let topBackground = UIColor(rgb: 0xf9f9f9)
+    }
+
+    class Image {
+        static let logoGitHub = UIImage(named: "logoLaunchScreen")
+    }
+}
