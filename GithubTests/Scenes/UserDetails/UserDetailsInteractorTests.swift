1037dcd19f085cdee9e6e0ef09057fb92e0f2e86
Fausto Castagnari Marouvo <fausto.marouvo@btgpactual.com>
2023-05-30 10:35:35 -0300

    Adição do host de testes e configutação da estrutura de um teste como base

--- GithubTests/Scenes/UserDetails/UserDetailsInteractorTests.swift
+++ GithubTests/Scenes/UserDetails/UserDetailsInteractorTests.swift
@@ -5,7 +5,7 @@
 //  Created by Fausto Marouvo on 30/05/23.
 //
 
-@testable import Github
+@testable import Fausto_Teste
 import RxCocoa
 import RxSwift
 import RxTest
@@ -14,8 +14,8 @@ import XCTest
 final class UserDetailsInteractorTests: XCTestCase {
     private var sut: UserDetailsInteractor!
     private var disposeBag: DisposeBag!
-    private let fetchUserDetailsUseCase: FetchUserDetailsUseCaseMock!
-    private let fetchUserRepositoriesUseCase: FetchUserRepositoriesUseCaseMock!
+    private var fetchUserDetailsUseCase: FetchUserDetailsUseCaseMock!
+    private var fetchUserRepositoriesUseCase: FetchUserRepositoriesUseCaseMock!
     private var userDetailsResponseExpected: UserDetailsResponse!
     private var userRepositoriesResponseExpected: UserRepositoriesResponse!
 
@@ -34,7 +34,7 @@ final class UserDetailsInteractorTests: XCTestCase {
         userRepositoriesResponseExpected = UserRepositoriesResponse(
             name: "name",
             description: "description",
-            update_at: "2023-05-07T20:51:28Z",
+            updated_at: "2023-05-07T20:51:28Z",
             watchers: 12
         )
     }
@@ -47,15 +47,15 @@ final class UserDetailsInteractorTests: XCTestCase {
         disposeBag = DisposeBag()
         fetchUserDetailsUseCase = FetchUserDetailsUseCaseMock()
         fetchUserRepositoriesUseCase = FetchUserRepositoriesUseCaseMock()
-        sut = HomeInteractor(
+        sut = UserDetailsInteractor(
             fetchUserDetailsUseCase: fetchUserDetailsUseCase,
             fetchUserRepositoriesUseCase: fetchUserRepositoriesUseCase
         )
     }
 
     func test_shouldFetchUserDetails_success() {
-        fetchUserDetailsUseCase.fetchUserDetailsReturnValue = Single.just(userDetailsResponseExpected)
-        sut.fetchUserDetails()
+        fetchUserDetailsUseCase.fetchUserDetailsLoginReturnValue = Single.just(userDetailsResponseExpected)
+        sut.fetchUserDetails(login: userDetailsResponseExpected.login)
             .subscribe(onSuccess: { _ in
                 XCTAssert(true)
             })
